9317ff07da798e3808c3783f899578488756c6d8
Juan Cruz Verdun <62451637+JCVerdun@users.noreply.github.com>
2024-12-02 08:26:01 -0300

    Funcionalidad Basica con plots

--- dicc.py
+++ dicc.py
@@ -42,15 +42,17 @@ def division(zt,z1,z2):
     zt['a']=float(z1['a']-z2['a'])
     Rec(zt)
 
+def resta(zt,z1,z2):
+    zt['x']=float(z1['x']-z2['x'])
+    zt['y']=float(z1['y']-z2['y'])
+    Pol(zt)
+
 def admitancia(y,z):
     y['r']=float(1/z['r'])
     y['a']=float(-1*z['a'])
     Rec(y)
 
-def iniciarVoltajes():
-    crearPol(urn,220,0)
-    crearPol(usn,220,240)
-    crearPol(utn,220,120)
+
 
 def desplazamiento():
     iro=dict()
@@ -66,35 +68,73 @@ def desplazamiento():
     division(uno,numerador,denominador)
 
 def mostrar(z):
-    if(z['x'] < 0.00000001):
-        z['x'] =0
-    if(z['y'] < 0.00000001):
-        z['y'] =0
-    if(z['r'] < 0.00000001):
-        z['r'] =0
-    print(f"{z['x']} ; {z['y']} <================> {z['r']} ; {z['a']*m.pi/180}")
+    tmp=dict()
+    tmp['x']=z['x']
+    tmp['y']=z['y']
+    tmp['r']=z['r']
+    tmp['a']=z['a']
+    if(tmp['x'] < 0.00000001):
+        tmp['x'] =0
+    if(tmp['y'] < 0.00000001):
+        tmp['y'] =0
+    if(tmp['r'] < 0.00000001):
+        tmp['r'] =0
+    print(f"{tmp['x']} ; {tmp['y']} <================> {tmp['r']} ; {tmp['a']*m.pi/180}")
 
-def plot():
+def plotFasorial():
     ax=plt.subplot()
     ax.quiver(0,0,float(urn['x']),float(urn['y']), angles='xy',scale_units='xy', scale=1, color="red")
     ax.quiver(0,0,float(usn['x']),float(usn['y']), angles='xy',scale_units='xy', scale=1, color="blue")
     ax.quiver(0,0,float(utn['x']),float(utn['y']), angles='xy',scale_units='xy', scale=1, color="green")
     ax.quiver(0,0,float(uno['x']),float(uno['y']), angles='xy',scale_units='xy', scale=1, color="black")
-    ax.set_xlim(-300,300)
-    ax.set_ylim(-300,300)
+    ax.quiver(float(uno['x']),float(uno['y']),float(uro['x']),float(uro['y']),angles='xy',scale_units='xy',scale=1,color="red")   
+    ax.quiver(float(uno['x']),float(uno['y']),float(uso['x']),float(uso['y']),angles='xy',scale_units='xy',scale=1,color="blue")
+    ax.quiver(float(uno['x']),float(uno['y']),float(uto['x']),float(uto['y']),angles='xy',scale_units='xy',scale=1,color="green")
+    ax.set_xlim(-800,800)
+    ax.set_ylim(-800,800)
     plt.xlabel("X")
     plt.xlabel("Y")
     plt.grid()
     plt.show()
 
+def plotTriangulo():
+    bx=plt.subplot()
+    bx.quiver(0,0,float(urs['x']),float(urs['y']),angles='xy',scale_units='xy',scale=1,color="red")
+    bx.quiver(float(urs['x']),float(urs['y']),float(ust['x']),float(ust['y']),angles='xy',scale_units='xy',scale=1,color="blue")
+    bx.quiver(float(ust['x']+urs['x']),float(ust['y']+urs['y']),float(utr['x']),float(utr['y']),angles='xy',scale_units='xy',scale=1,color="green")
 
+
+    bx.quiver(0,0,float(uro['x']),float(uro['y']),angles='xy',scale_units='xy',scale=1,color="orange")
+    bx.quiver(float(urs['x']),float(urs['y']),float(uso['x']),float(uso['y']),angles='xy',scale_units='xy',scale=1,color="lightblue")
+    bx.quiver(float(ust['x']+urs['x']),float(ust['y']+urs['y']),float(uto['x']),float(uto['y']),angles='xy',scale_units='xy',scale=1,color="lightgreen")
+    
+    bx.quiver(0,0,float(urn['x']),float(urn['y']),angles='xy',scale_units='xy',scale=1,color="grey")
+    bx.quiver(float(urs['x']),float(urs['y']),float(usn['x']),float(usn['y']),angles='xy',scale_units='xy',scale=1,color="grey")
+    bx.quiver(float(ust['x']+urs['x']),float(ust['y']+urs['y']),float(utn['x']),float(utn['y']),angles='xy',scale_units='xy',scale=1,color="grey")
+
+    bx.quiver(float(urn['x']),float(urn['y']),float(uno['x']),float(uno['y']),angles='xy',scale_units='xy',scale=1,color="black")
+    
+    bx.set_xlim(-100,450)
+    bx.set_ylim(-450,450)
+    plt.show()
+
+def iniciarVoltajes():
+    crearPol(urn,220,0)
+    crearPol(usn,220,240)
+    crearPol(utn,220,120)
+    resta(urs,urn,usn)
+    resta(ust,usn,utn)
+    resta(utr,utn,urn)
+    
 
 
 urn=dict()
 usn=dict()
 utn=dict()
+urs=dict()
+ust=dict()
+utr=dict()
 iniciarVoltajes()
-print(urn)
 
 
 zr=dict()
@@ -134,6 +174,11 @@ if(opcionImp == 2):
     y=float(input("Ingrese el valor de la parte imaginaria de Zt: "))
     print(f"los valores ingresador son: {x}, {y}")
     crearRec(zt,x,y)
+if(opcionImp==3):
+    crearRec(zr,15,20)
+    crearRec(zs,15,-15)
+    crearRec(zt,25,40)
+    
 
 yr=dict()
 ys=dict()
@@ -143,9 +188,15 @@ admitancia(ys,zs)
 admitancia(yt,zt)
 uno=dict()
 desplazamiento()
-print(uno)
 mostrar(uno)
-plot()
+uro=dict()
+uso=dict()
+uto=dict()
+suma(uro,urn,uno)
+suma(uso,usn,uno)
+suma(uto,utn,uno)
+plotFasorial()
+plotTriangulo()
 
 
 #crearRec(zr,3,4)
